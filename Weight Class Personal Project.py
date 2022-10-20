9f0399f078c651d0571520844003de6495e3b813
cheng151 <83590915+cheng151@users.noreply.github.com>
2022-10-19 23:23:21 -0400

    improving

--- Weight Class Personal Project.py
+++ Weight Class Personal Project.py
@@ -1,35 +1,41 @@
+class MenWeight:
+    """This class is for men's UFC weight-classes"""
+    def __init__(self, m_weight):
+        self.m_weight = m_weight
 
-user_weight = float(input("Enter your weight: "))
+    def get_m_weight(self):
+        """ Categortizing weight classes"""
+        if self.m_weight <= 115:
+            return ("You are strawweight.")
+        elif self.m_weight <= 125:
+            print("You are flyweight.")
+        elif self.m_weight <= 135:
+            print("You are bantamweight.")
+        elif self.m_weight <= 145:
+            print("You are featherweight.")
+        elif self.m_weight <= 155:
+            print("You are lightweight.")
+        elif self.m_weight <= 165:
+            print("You are super lightweight.")
+        elif self.m_weight <= 170:
+            print("You are welterweight.")
+        elif self.m_weight <= 175:
+            print("You are super welterweight.")
+        elif self.m_weight <= 185:
+            print("You are middleweight.")
+        elif self.m_weight <= 195:
+            print("You are super middleweight.")
+        elif self.m_weight <= 205:
+            print("You are light heavyweight.")
+        elif self.m_weight <= 225:
+            print("You are cruiserweight.")
+        elif self.m_weight <= 265:
+            print("You are heavyweight.")
+        elif self.m_weight > 265:
+            print("You are super heavyweight.")
+        else:
+            print("Please enter a valid weight.")
 
+print(MenWeight(110))
 
-if user_weight <= 115:
-    print("You are strawweight.")
-elif user_weight <= 125:
-    print("You are flyweight.")
-elif user_weight <= 135:
-    print("You are bantamweight.")
-elif user_weight <= 145:
-    print("You are featherweight.")
-elif user_weight <= 155:
-    print("You are lightweight.")
-elif user_weight <= 165:
-    print("You are super lightweight.")
-elif user_weight <= 170:
-    print("You are welterweight.")
-elif user_weight <= 175:
-    print("You are super welterweight.")
-elif user_weight <= 185:
-    print("You are middleweight.")
-elif user_weight <= 195:
-    print("You are super middleweight.")
-elif user_weight <= 205:
-    print("You are light heavyweight.")
-elif user_weight <= 225:
-    print("You are cruiserweight.")
-elif user_weight <= 265:
-    print("You are heavyweight.")
-elif user_weight > 265:
-    print("You are super heavyweight.")
-else:
-    print("Please enter a valid weight.")
-
+### put user input in def main(): function
